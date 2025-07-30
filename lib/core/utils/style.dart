5e110e9445b16a5347c120032c10adab8a49b5a6
hageromar <hageromar328@gmail.com>
2025-07-30 12:59:15 +0300

    add all style

---
+++ lib/core/utils/style.dart
@@ -0,0 +1,59 @@
+import 'package:flutter/material.dart';
+abstract class Styles {
+  static TextStyle style24 = const TextStyle(
+    color: Colors.black,
+    fontWeight: FontWeight.w600,
+    fontFamily: 'Inter',
+    height: 0,
+    fontSize: 24
+  );
+  static TextStyle style20 = const TextStyle(
+    color: Colors.black,
+    fontWeight: FontWeight.w600,
+    fontFamily: 'Inter',
+    height: 0,
+    fontSize: 20
+  );
+  static TextStyle style18 = const TextStyle(
+    color: Colors.black,
+    fontWeight: FontWeight.w400,
+    fontFamily: 'Inter',
+    height: 0,
+    fontSize: 18
+  );
+  static TextStyle style22 = const TextStyle(
+    color: Colors.black,
+    fontWeight: FontWeight.w500,
+    fontFamily: 'Inter',
+    height: 0,
+    fontSize: 22
+  );
+  static TextStyle style25 = const TextStyle(
+    color: Colors.black,
+    fontWeight: FontWeight.w500,
+    fontFamily: 'Inter',
+    height: 0,
+    fontSize: 25
+  );
+  static TextStyle stylesemibold18 = const TextStyle(
+    color: Colors.black,
+    fontWeight: FontWeight.w600,
+    fontFamily: 'Inter',
+    height: 0,
+    fontSize: 18
+  );
+  static TextStyle stylesemibold20 = const TextStyle(
+    color: Colors.black,
+    fontWeight: FontWeight.w400,
+    fontFamily: 'Inter',
+    height: 0,
+    fontSize: 20
+  );
+  static TextStyle stylesemibold25 = const TextStyle(
+    color: Colors.black,
+    fontWeight: FontWeight.w600,
+    fontFamily: 'Inter',
+    height: 0,
+    fontSize: 18
+  );
+}
